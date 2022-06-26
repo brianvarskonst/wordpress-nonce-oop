0b9081a63e16a3904f33a3ba35075c1953cefe80
brianvarskonst <burnify.design@gmail.com>
2022-06-26 19:16:19 +0000

    Add creational Testee Tests

--- tests/Unit/NonceManagerTest.php
+++ tests/Unit/NonceManagerTest.php
@@ -6,15 +6,25 @@ namespace Bvsk\WordPress\NonceManager\Tests\Unit;
 
 use Bvsk\WordPress\NonceManager\NonceManager;
 use Bvsk\WordPress\NonceManager\Nonces\Factory\DefaultNonceProperties;
+use Bvsk\WordPress\NonceManager\Nonces\Factory\NonceFactory;
 use Bvsk\WordPress\NonceManager\Nonces\FieldNonce;
+use Bvsk\WordPress\NonceManager\Nonces\Nonce;
 use Bvsk\WordPress\NonceManager\Nonces\SimpleNonce;
 use Bvsk\WordPress\NonceManager\Nonces\UrlNonce;
 use Bvsk\WordPress\NonceManager\Tests\UnitTestCase;
+use Bvsk\WordPress\NonceManager\Verification\Verifier;
 use function Brain\Monkey\Functions\expect;
 
 class NonceManagerTest extends UnitTestCase
 {
-    public function testCreateNonceManager(): void
+    public function testCreateNewInstanceOfNonceManagerWithDependencyInjection(): void
+    {
+        $testee = $this->buildTesteeWithAnonymousDependencies();
+
+        $this->assertInstanceOf(NonceManager::class, $testee);
+    }
+
+    public function testCreateNewInstanceOfNonceManagerFromDefaults(): void
     {
         $testee = $this->buildTestee();
 
@@ -104,5 +114,64 @@ class NonceManagerTest extends UnitTestCase
     private function buildTestee(): NonceManager
     {
         return NonceManager::createFromDefaults();
+    }
+
+    private function buildTesteeWithAnonymousDependencies(): NonceManager
+    {
+        return new NonceManager(
+            new class implements NonceFactory {
+
+                public function accepts(string $type, array $data): bool
+                {
+                    return true;
+                }
+
+                public function getSupportedType(): string
+                {
+                    return 'FooBar';
+                }
+
+                public function create(string $type, array $data = []): Nonce
+                {
+                    return new class implements Nonce {
+
+                        public function getAction(): string
+                        {
+                            return 'foo';
+                        }
+
+                        public function getLifetime(bool $actualLifetime = true): int
+                        {
+                            return 1;
+                        }
+
+                        public function getRequestName(): string
+                        {
+                            return 'bar';
+                        }
+
+                        public function getToken(): string
+                        {
+                            return 'fooBar';
+                        }
+                    };
+                }
+            },
+            new class implements Verifier {
+
+                public function verify(Nonce $nonce): bool
+                {
+                    return true;
+                }
+
+                public function getAge(Nonce $nonce): string
+                {
+                    return '1';
+                }
+
+                public function renderMessageHasExpired(Nonce $nonce): void
+                {}
+            }
+        );
     }
 }
